52790c3096efd2ac27cc2b9adbda63323d1c0c82
Wagner <wmaluk@gmail.com>
2022-06-28 15:16:52 -0300

    add test project

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { ITarefa } from "../../types/tarefa";
+import Botao from "../Botao";
+import style from "./Formulario.module.scss";
+import {v4 as uuidv4} from 'uuid'
+
+interface Props {
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}
+
+export default function Formulario({ setTarefas }: Props) {
+    const [tarefa, setTarefa] = useState('')
+    const [tempo, setTempo] = useState('00:00:00')
+    function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        setTarefas((tarefas) => 
+            [
+                ...tarefas, 
+                { 
+                    tarefa,
+                    tempo,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4()
+                }
+            ]
+        );
+        setTarefa("")
+        setTempo("00:00:00")
+      }
+    return (
+        <form
+          className={style.novaTarefa}
+          onSubmit={adicionarTarefa}
+        >
+          <div className={style.inputContainer}>
+            <label htmlFor="tarefa">Adicione um novo estudo</label>
+            <input
+              type="text"
+              name="tarefa"
+              value={tarefa}
+              onChange={evento => setTarefa(evento.target.value)}
+              id="tarefa"
+              placeholder="O que voce quer estudar"
+              required
+            />
+          </div>
+          <div className={style.inputContainer}>
+            <label htmlFor="tempo">Tempo</label>
+            <input
+              type="time"
+              step="1"
+              name="tempo"
+              value={tempo}
+              onChange={evento => setTempo(evento.target.value)}
+              min="00:00:00"
+              max="01:30:00"
+            />
+          </div>
+          <Botao type="submit">Adicionar</Botao>
+        </form>
+      );
+}
